e626c8a1697721d251cec99f4c1899c220a0cfbd
dev-langesh <dev.langesh@gmail.com>
2022-06-26 21:43:52 +0530

    validate name

---
+++ test/name.test.js
@@ -0,0 +1,15 @@
+const { validateName } = require("..");
+
+describe("validate name", () => {
+  test("should return true", () => {
+    expect(validateName("langesh_-")).toBe(true);
+  });
+
+  test("should return false", () => {
+    expect(validateName("langesh@123")).toBe(false);
+  });
+
+  test("should return false", () => {
+    expect(validateName("langesh***")).toBe(false);
+  });
+});
